bd502d8ddce57993fdbe09386645b67e7fc8167a
Mauro Rivero <126116175+mauro-rb@users.noreply.github.com>
2024-08-08 22:02:27 -0300

    Update ProductoSalidaDto.java

--- webExplora/webExplora/src/main/java/com/backend/webExplora/dto/salida/ProductoSalidaDto.java
+++ webExplora/webExplora/src/main/java/com/backend/webExplora/dto/salida/ProductoSalidaDto.java
@@ -10,22 +10,30 @@ public class ProductoSalidaDto {
     private Long id;
     private String nombre;
     private String descripcion;
+    private String descripcion_larga;
     private String imagenUrl;
+    private String imagenUrl2;
+    private String imagenUrl3;
     private BigDecimal precio;
     private Boolean disponible;
     private String ubicacion;
+    private String itinerario;
 
     public ProductoSalidaDto() {
     }
 
-    public ProductoSalidaDto(Long id, String nombre, String descripcion, String imagenUrl, BigDecimal precio, Boolean disponible, String ubicacion) {
+    public ProductoSalidaDto(Long id, String itinerario, String nombre, String descripcion, String descripcion_larga, String imagenUrl, String imagenUrl2, String imagenUrl3, BigDecimal precio, Boolean disponible, String ubicacion) {
         this.id = id;
         this.nombre = nombre;
         this.descripcion = descripcion;
+        this.descripcion_larga = descripcion_larga;
         this.imagenUrl = imagenUrl;
         this.precio = precio;
         this.disponible = disponible;
         this.ubicacion = ubicacion;
+        this.imagenUrl2 = imagenUrl2;
+        this.imagenUrl3 = imagenUrl3;
+        this.itinerario = itinerario;
     }
 }
 
