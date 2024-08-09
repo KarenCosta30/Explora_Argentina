3b99498c3d4f37a4bc91cb46a7672a5394ddb88b
juanscaffo <juanscaffo@hotmail.com>
2024-08-09 13:10:52 -0300

    pagina lista para chequear requerimientos a cumplir

--- PIG3/PI-G3/src/Components/CardTour.jsx
+++ PIG3/PI-G3/src/Components/CardTour.jsx
@@ -8,14 +8,17 @@ const CardTour = ({item}) => {
     
       <div className='card-tour'>
         <img className='img-card-tour' src={imagenUrl} alt="img-jujuy" /> {/* traer img */}
+        <div className="info-card-tour">
         <p className='title-card-tour'>{nombre}</p> {/* TRAER DE ENDPOINT */}
         <p className='subtitle-card-tour'>{descripcion}</p>{/* TRAER DE ENDPOINT */}
         <div className="price-card-tour">
           <p>Desde:<span className='price'> ${precio}</span></p>{/* TRAER DE ENDPOINT */}
-          <Link to={`/detail/${id}`} className="link-card-tour">
+          <Link to={`/detail/${id}`}>
             <Button className="btn-card-tour">Detalles</Button>
           </Link>
         </div>
+        </div>
+       
       </div>
    
   );
