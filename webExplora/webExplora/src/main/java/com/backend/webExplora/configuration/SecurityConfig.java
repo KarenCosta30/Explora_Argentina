28405bec7172abe2be19e253ee2125a0119ee519
Mauro Rivero <126116175+mauro-rb@users.noreply.github.com>
2024-08-16 22:34:58 -0300

    Corrigiendo permisos Cors

--- webExplora/webExplora/src/main/java/com/backend/webExplora/configuration/SecurityConfig.java
+++ webExplora/webExplora/src/main/java/com/backend/webExplora/configuration/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.backend.webExplora.configuration;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +11,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.firewall.HttpFirewall;
 import org.springframework.security.web.firewall.StrictHttpFirewall;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -20,10 +25,15 @@ public class SecurityConfig {
             .csrf().disable()
             .authorizeRequests()
                 .antMatchers("/usuarios/registrar").permitAll()
-                .anyRequest().authenticated();
+                .antMatchers("/api/productos/aleatorios").permitAll() // Permitir acceso sin autenticación
+                .anyRequest().authenticated()
+            .and()
+            .cors() // Asegúrate de habilitar CORS
+            .and()
+            .formLogin().disable() // Deshabilitar formulario de inicio de sesión si no se usa
+            .httpBasic().disable(); // Deshabilitar autenticación básica si no se usa
         return http.build();
     }
-    
 
     @Bean
     public HttpFirewall allowUrlEncodedSlashHttpFirewall() {
@@ -36,5 +46,16 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // Permitir orígenes específicos
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Permitir métodos HTTP
+        configuration.setAllowedHeaders(List.of("*")); // Permitir todos los headers
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
