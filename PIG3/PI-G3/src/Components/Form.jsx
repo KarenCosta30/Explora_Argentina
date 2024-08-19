d5b136c0c1ed8a38d061e5510d4dd4244302f484
mauro-rb <mauro27.rb@gmail.com>
2024-08-19 18:32:16 -0300

    Agregando estilos al formulario de registro

--- PIG3/PI-G3/src/Components/Form.jsx
+++ PIG3/PI-G3/src/Components/Form.jsx
@@ -1,24 +1,46 @@
 import React from "react";
 import Button from "./Button";
 
-const Form = ({ fields, buttonText, onSubmit,className,inputClassName,children }) => {
+const Form = ({ fields, buttonText, onSubmit, className, inputClassName, children , paragraphText  }) => {
   return (
     <div>
       <form onSubmit={onSubmit} className={className}>
         {children}
-        {fields.map((field, index) => ( // fields que es la primer props recibira una lista de objetos que definiran los campos del formulario
-          <input
-            key={index}
-            className={inputClassName}
-            type={field.type}
-            placeholder={field.placeholder}
-            name={field.name}
-            value={field.value}
-            onChange={field.onChange}
-          />
-        ))}
-         
-        <Button className={"btn-search"}>{buttonText}</Button> 
+        {fields.map((field, index) => {
+          if (field.type === "group") {
+            // Si el field es un grupo, renderiza un div con la clase especificada
+            return (
+              <div key={index} className={field.className}>
+                {field.fields.map((subField, subIndex) => (
+                  <input
+                    key={subIndex}
+                    className={inputClassName}
+                    type={subField.type}
+                    placeholder={subField.placeholder}
+                    name={subField.name}
+                    value={subField.value}
+                    onChange={subField.onChange}
+                  />
+                ))}
+              </div>
+            );
+          } else {
+            // Si el field no es un grupo, renderiza un input normal
+            return (
+              <input
+                key={index}
+                className={inputClassName}
+                type={field.type}
+                placeholder={field.placeholder}
+                name={field.name}
+                value={field.value}
+                onChange={field.onChange}
+              />
+            );
+          }
+        })}
+        {paragraphText && <p className="form-paragraph">{paragraphText}</p>}
+        <Button className={"btn-search"}>{buttonText}</Button>
       </form>
     </div>
   );
--- PIG3/PI-G3/src/Page/CreateAccount.jsx
+++ PIG3/PI-G3/src/Page/CreateAccount.jsx
@@ -71,27 +71,68 @@ const CreateAccount = () => {
   };
 
   return (
-    <main className='main'>
-      <div className='container-form-create'>
-        <p className='title-form-create'>Registrate</p>
-        <p className='subtitle-form-create'>Es rápido y fácil</p>
-        <div className="check">
-          <input type="checkbox" />
-          <label>Quiero ser usuario administrador</label>
-        </div>
-        <Form className={"form-create-accout"}
+    <main className="main">
+      <div className="container-form-create">
+        <p className="title-form-create">Registrate</p>
+        <p className="subtitle-form-create">Es rápido y fácil</p>
+        <hr className="divider" />
+        <Form
+          className={"form-create-accout"}
           fields={[
-            { type: "text", placeholder: "Nombre", name: "nombre", value: formData.nombre, onChange: handleChange },
-            { type: "text", placeholder: "Apellido", name: "apellido", value: formData.apellido, onChange: handleChange },
-            { type: "email", placeholder: "Correo electrónico", name: "email", value: formData.email, onChange: handleChange },
-            { type: "password", placeholder: "Password", name: "password", value: formData.password, onChange: handleChange },
-            { type: "password", placeholder: "Confirma tu password", name: "confirmarPassword", value: formData.confirmarPassword, onChange: handleChange },
+            {
+              type: "group",
+              className: "form-create-accout-row",
+              fields: [
+                {
+                  type: "text",
+                  placeholder: "Nombre",
+                  name: "nombre",
+                  value: formData.nombre,
+                  onChange: handleChange,
+                },
+                {
+                  type: "text",
+                  placeholder: "Apellido",
+                  name: "apellido",
+                  value: formData.apellido,
+                  onChange: handleChange,
+                },
+              ],
+            },
+            {
+              type: "text",
+              placeholder: "Correo electrónico",
+              name: "email",
+              value: formData.email,
+              onChange: handleChange,
+            },
+            {
+              type: "password",
+              placeholder: "Contraseña",
+              name: "password",
+              value: formData.password,
+              onChange: handleChange,
+            },
+            {
+              type: "password",
+              placeholder: "Confirma la contraseña",
+              name: "confirmarPassword",
+              value: formData.confirmarPassword,
+              onChange: handleChange,
+            },
           ]}
-          buttonText="Crear Cuenta"
+          paragraphText="Al hacer clic en Registrarte, aceptas las Condiciones, la Política de privacidad y la Política de cookies."
+          buttonText="Registrarse"
           onSubmit={handleSubmit}
           inputClassName="create-account-input"
         />
-        <p className='p-end'>¿Ya tienes una cuenta? <Link to={"/login"}><span className='span'> Iniciar Sesión</span></Link></p>
+
+        <p className="p-end">
+          ¿Ya tienes una cuenta?{" "}
+          <Link to={"/login"}>
+            <span className="span">Iniciar Sesión</span>
+          </Link>
+        </p>
         {error && <p className="error-message">{error}</p>}
       </div>
     </main>
@@ -99,103 +140,3 @@ const CreateAccount = () => {
 };
 
 export default CreateAccount;
-/*
-import React, { useState } from 'react';
-import Form from "../Components/Form";
-import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
-
-const CreateAccount = () => {
-  const [error, setError] = useState(null);
-  const [formData, setFormData] = useState({
-    nombre: "",
-    apellido: "",
-    email: "",
-    password: "",
-    confirmarPassword: ""
-  });
-
-  const navigate = useNavigate(); // Hook de react-router para redirigir
-
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const { nombre, apellido, email, password, confirmarPassword } = formData;
-
-    if (!nombre || !/^[a-zA-Z]+$/.test(nombre) || nombre.length <= 1) {
-      setError("Nombre incorrecto");
-      return;
-    }
-
-    if (!apellido || !/^[a-zA-Z]+$/.test(apellido) || apellido.length <= 1) {
-      setError("Apellido incorrecto");
-      return;
-    }
-
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-      setError("Debe ingresar un correo electrónico válido.");
-      return;
-    }
-
-    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,16}$/;
-    if (!passwordRegex.test(password) || /\s/.test(password)) {
-      setError("Error en la contraseña");
-      return;
-    }
-
-    if (password !== confirmarPassword) {
-      setError("Las contraseñas no coinciden.");
-      return;
-    }
-
-    setError(null);
-
-    try {
-      await axios.post('http://localhost:8081/usuarios/registrar', {
-        nombre,
-        apellido,
-        email,
-        password,
-        esAdministrador: false
-      });
-      navigate('/login'); // Redirige al login después de un registro exitoso
-    } catch (err) {
-      setError("Error al registrar el usuario. Inténtalo de nuevo.");
-      console.error(err);
-    }
-  };
-
-  return (
-    <main className='main'>
-      <div className='container-form-create'>
-        <p className='title-form-create'>Regístrate</p>
-        <p className='subtitle-form-create'>Es rápido y fácil</p>
-        <Form 
-          className={"form-create-accout"} // Asegúrate de que esta clase exista en tu CSS
-          inputClassName="create-account-input" // Asegúrate de que esta clase exista en tu CSS
-          fields={[
-            { type: "text", placeholder: "Nombre", name: "nombre", value: formData.nombre, onChange: handleChange },
-            { type: "text", placeholder: "Apellido", name: "apellido", value: formData.apellido, onChange: handleChange },
-            { type: "email", placeholder: "Correo electrónico", name: "email", value: formData.email, onChange: handleChange },
-            { type: "password", placeholder: "Contraseña", name: "password", value: formData.password, onChange: handleChange },
-            { type: "password", placeholder: "Confirma tu Contraseña", name: "confirmarPassword", value: formData.confirmarPassword, onChange: handleChange }
-          ]}
-          buttonText="Crear Cuenta"
-          onSubmit={handleSubmit}
-        />
-        <p className='p-end'>¿Ya tienes una cuenta? <Link to={"/login"}><span className='span'> Iniciar Sesión</span></Link></p>
-        {error && <p className="error-message">{error}</p>}
-      </div>
-    </main>
-  );
-};
-
-export default CreateAccount;
-*/
