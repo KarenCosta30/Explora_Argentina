eca32bf18743c410973a8a505e26a3e6707904a5
juanscaffo <juanscaffo@hotmail.com>
2024-08-26 20:12:25 -0300

    pagina para sprint 2 terminada

--- PIG3/PI-G3/src/Components/MapComponent.jsx
+++ PIG3/PI-G3/src/Components/MapComponent.jsx
@@ -36,10 +36,10 @@ const MapComponent = () => {
   const position = [tour.latitud, tour.longitud];
 
   return (
-    <MapContainer center={position} zoom={13} style={{ height: '280px', width: '100%' }}>
+    <MapContainer center={position} zoom={13} style={{ height: '280px', width: '100%' }} zoomControl={false}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">'
       />
       <Marker position={position}>
         <Popup>
