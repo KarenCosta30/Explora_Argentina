b21ee118438d0547a8afedbce39e13fdb28bf1b7
Michael Irigoyen <mkeviniriogoyen@gmail.com>
2024-09-02 18:41:09 -0300

    Controladores totales de producto

--- webExplora/webExplora/src/main/java/com/backend/webExplora/controller/ProductoController.java
+++ webExplora/webExplora/src/main/java/com/backend/webExplora/controller/ProductoController.java
@@ -2,8 +2,12 @@ package com.backend.webExplora.controller;
 
 import com.backend.webExplora.dto.entrada.AsignarCategoriaProducto;
 import com.backend.webExplora.dto.entrada.ProductoEntradaDto;
+import com.backend.webExplora.dto.salida.CategoriaSalidaDto;
 import com.backend.webExplora.dto.salida.ProductoSalidaDto;
 import com.backend.webExplora.service.IProductoService;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -33,5 +37,25 @@ public class ProductoController {
     public ResponseEntity<ProductoSalidaDto> agregarProducto(@RequestBody ProductoEntradaDto productoDto) {
         ProductoSalidaDto nuevoProducto = productoService.registrarProducto(productoDto);
         return ResponseEntity.ok(nuevoProducto);
+    }
+    @GetMapping("/aleatorios")
+    public ResponseEntity<List<ProductoSalidaDto>> obtenerProductosAleatorios() {
+        List<ProductoSalidaDto> productos = productoService.obtenerProductosAleatorios();
+        return ResponseEntity.ok(productos);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductoSalidaDto> obtenerDetalleProducto(@PathVariable Long id) {
+        ProductoSalidaDto producto = productoService.obtenerDetalleProducto(id);
+        return ResponseEntity.ok(producto);
+    }
+    @GetMapping("/categoria/{categoriaId}") 
+    public ResponseEntity<List<ProductoSalidaDto>> obtenerProductosPorCategoria(@PathVariable Long categoriaId) {
+        List<ProductoSalidaDto> productos = productoService.obtenerProductosPorCategoria(categoriaId);
+        return ResponseEntity.ok(productos);
+    }
+        @GetMapping("/categorias/aleatorias")
+    public ResponseEntity<List<CategoriaSalidaDto>> obtenerCategoriasAleatorias() {
+        List<CategoriaSalidaDto> categorias = productoService.obtenerCategoriasAleatorias();
+        return ResponseEntity.ok(categorias);
     }
 }
